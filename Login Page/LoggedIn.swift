bc97c241158bf775b378f35a78305caad696f177
Jamie Mills <hyojoo.mills@gmail.com>
2021-07-25 20:51:51 +1000

    first commit and pushing it to GitHub

---
+++ Login Page/LoggedIn.swift
@@ -0,0 +1,18 @@
+//
+//  LoggedIn.swift
+//  Login Page
+//
+//  Created by Hyojoo Mills on 19/7/21.
+//
+
+import UIKit
+
+class LoggedIn: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ Login Page/SignUp.swift
@@ -0,0 +1,19 @@
+//
+//  SignUp.swift
+//  Login Page
+//
+//  Created by Hyojoo Mills on 19/7/21.
+//
+
+import UIKit
+
+class SignUp: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
